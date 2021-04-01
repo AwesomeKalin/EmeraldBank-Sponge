ea7f0991ecf6750e01580fae56abfa171005f4af
AwesomeKalin <47223207+AwesomeKalin@users.noreply.github.com>
2021-04-01 16:35:01 +0100

    Bumped version to 1.0-BETA

--- src/main/java/com/github/AwesomeKalin/EmeraldBankSponge/commands/Amount.java
+++ src/main/java/com/github/AwesomeKalin/EmeraldBankSponge/commands/Amount.java
@@ -2,7 +2,6 @@ package com.github.AwesomeKalin.EmeraldBankSponge.commands;
 
 import com.github.AwesomeKalin.EmeraldBankSponge.ConfigurationManager;
 import com.github.AwesomeKalin.EmeraldBankSponge.api.GetBankPlace;
-import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
@@ -14,7 +13,7 @@ import java.util.List;
 
 public class Amount implements CommandExecutor {
     @Override
-    public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
+    public CommandResult execute(CommandSource src, CommandContext args) {
         if(!(src instanceof Player)){
             src.sendMessage(Text.of("Must be run as player!!!"));
             return CommandResult.success();
@@ -25,7 +24,6 @@ public class Amount implements CommandExecutor {
         }
         String name = args.<String>getOne("name").get();
         ConfigurationManager config = ConfigurationManager.getInstance();
-        List<String> bankNames = (List<String>) config.getConfig().getNode("bank.banks").getValue();
         List<Integer> bankEmeralds = (List<Integer>) config.getConfig().getNode("bank.emeralds").getValue();
         List<Integer> bankIron = (List<Integer>) config.getConfig().getNode("bank.iron").getValue();
         int place = GetBankPlace.getBankPlace(name);
@@ -33,7 +31,9 @@ public class Amount implements CommandExecutor {
             src.sendMessage(Text.of("&4Bank doesn't exist!"));
             return CommandResult.success();
         }
+        assert bankEmeralds != null;
         Integer emeralds = bankEmeralds.get(place);
+        assert bankIron != null;
         Integer iron = bankIron.get(place);
         src.sendMessage(Text.of("&aInformation for bank, " + name + ":"));
         src.sendMessage(Text.of("&aEmeralds: " + emeralds));
